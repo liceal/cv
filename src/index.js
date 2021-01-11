289f0709181cff91c17d7340076ccceda6c60392
liceal <675024132@qq.com>
2021-01-11 15:13:34 +0800

    优化代码

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import {HashRouter, Switch, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import routers from './routers/routers'
 import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 
 ReactDOM.render(
   <HashRouter>
+    <Header />
     <Switch>
       {routers.map(v => (
         <Route key={v.path} path={v.path} exact={v.exact} component={v.component}/>
