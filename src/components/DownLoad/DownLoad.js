16647039ad94c044779581fa436a093821853018
liceal <675024132@qq.com>
2021-10-25 16:54:17 +0800

    更改pdf简历

--- src/components/DownLoad/DownLoad.js
+++ src/components/DownLoad/DownLoad.js
@@ -7,7 +7,7 @@ class DownLoad extends PureComponent {
     this.downLoadUrl = this.downLoadUrl.bind(this);
   }
   downLoadUrl() {
-    window.open(require('@/static/wordCv.pdf'))
+    window.open(require('@/static/cv.pdf'))
   }
   render() {
 
