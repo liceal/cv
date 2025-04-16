89f3a0453cf6abcdc361e6f62d0ce4453338f8df
liceal <liceal@qq.com>
2025-04-16 22:11:37 +0800

    fix: 去掉旧简历地址

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,16 +1,19 @@
 import loadable from '@loadable/component'
-const routers = [{
-    path: '/',
-    exact: true, //精准
-    component: loadable(() =>
-        import('../containers/markdown-cv/markdown-cv.js'))
+const routers = [
+    {
+        path: '/',
+        exact: true, //精准
+        component: loadable(() =>
+            import('../containers/markdown-cv/markdown-cv.js'))
 
-}, {
-    path: '/static-cv',
-    exact: true, //精准
-    component: loadable(() =>
-        import('../containers/Cv/Cv.js'))
-}
+    },
+    // {
+    //     path: '/static-cv',
+    //     exact: true, //精准
+    //     component: loadable(() =>
+    //         import('../containers/Cv/Cv.js'))
+    // }
+
 ]
 
 export default routers
